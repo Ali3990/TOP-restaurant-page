eaf10cf225c91f0c01a8dde8d3fb45edc45ae425
AliMMCR <ali@mmcrealestate.com>
2026-08-19 17:45:50 -0700

    added a telephone number

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,6 +12,11 @@ export function createContactTab(){
     address.innerHTML = "Location: 88 Dragon Gate Lane · Wulin District · Beyond the Jade Mountains · Forbidden Kingdom 88888"
     document.querySelector("#content").append(address);
 
+    const telephone = document.createElement('div');
+    telephone.classList.add("telephone");
+    address.innerHTML = "Telephone: (888) 569-1337";
+    document.querySelector("#content").append(telephone);
+
     const schedule = document.createElement('ul');
     schedule.innerHTML = `<p>Holiday Hours subject to change: </p>
                             <p> Mon-Fri : 9am - 9pm </p>
